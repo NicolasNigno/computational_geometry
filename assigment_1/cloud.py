08864a4c277b607dcc43b0eaad91fd8aa99514b4
Nicolas Nino <nicolasnigno@gmail.com>
2021-08-10 21:32:44 -0500

    folder

--- assigment_1/cloud.py
+++ assigment_1/cloud.py
@@ -1,4 +1,9 @@
 import numpy as np
 
-def uniformCloud(size):
-    return np.random.uniform(low=-100.0, high=100.0, size=(size, 2))
+def uniformCloud(low, high, size):
+    assert high > low, 'papi qué putas'
+
+    return np.random.uniform(low=low, high=high, size=(size, 2))
+
+def gaussianCloud(loc, scale, size):
+    return np.random.normal(loc=loc, scale=scale, size=(size, 2))
--- cloud.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def uniformCloud(low, high, size):
-    assert high > low, 'papi qué putas'
-
-    return np.random.uniform(low=low, high=high, size=(size, 2))
-
-def gaussianCloud(loc, scale, size):
-    return np.random.normal(loc=loc, scale=scale, size=(size, 2))
