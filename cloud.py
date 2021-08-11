1bc01e048ec34ec9b02a9987353f9ed871378305
Nicolas Nino <nicolasnigno@gmail.com>
2021-08-10 21:26:59 -0500

    gaussian

--- cloud.py
+++ cloud.py
@@ -4,3 +4,6 @@ def uniformCloud(low, high, size):
     assert high > low, 'papi qué putas'
 
     return np.random.uniform(low=low, high=high, size=(size, 2))
+
+def gaussianCloud(loc, scale, size):
+    return np.random.normal(loc=loc, scale=scale, size=(size, 2))
