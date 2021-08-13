6ecf6c6d9d5922efd7b4fdf627c3f75314e52013
Juan Vivas <jdvivasf1@gmail.com>
2021-08-12 22:27:19 -0500

    Avance con incremental, aun bug de no giro

---
+++ assigment_1/Incremental.py
@@ -0,0 +1,65 @@
+from cloud import uniformCloud
+import numpy as np
+from matplotlib import pyplot as plt
+
+cloud_1 = uniformCloud(10,100,100)
+
+def cruz_crit(array1):
+    px = array1[-3][0]
+    qx = array1[-2][0]
+    rx = array1[-1][0]
+    py = array1[-3][1]
+    qy = array1[-2][1]
+    ry = array1[-1][1]
+    
+    return ((qx - px) * (ry - py)) - ((qy - py) * (rx - px))
+    
+
+def Incremental(cloud):
+    
+    convexhull_up = np.empty([0,2])
+    convexhull_lw = np.empty([0,2])
+    
+    ##Upper Hull
+    cloud.view('f8,f8').sort(order=['f0','f1'], axis=0)
+    
+    for i in range(len(cloud)):
+        
+        new = np.array([[cloud[i][0],cloud[i][1]]])
+        convexhull_up = np.append(convexhull_up,new,axis=0)
+        
+        try:
+            cruz = cruz_crit(convexhull_up) 
+        except IndexError:
+            cruz = np.nan
+        
+        while len(convexhull_up) > 2 and cruz <= 0:
+            convexhull_up = np.delete(convexhull_up, -2, axis=0)
+            
+
+    ##Lower Hull
+    cloud.view('f8,f8')[::-1].sort(order=['f0','f1'], axis=0)
+    
+    for i in range(len(cloud)):
+        
+        new = np.array([[cloud[i][0],cloud[i][1]]])
+        convexhull_lw = np.append(convexhull_lw,new,axis=0)
+        
+        try:
+            cruz = cruz_crit(convexhull_lw) 
+        except IndexError:
+            cruz = np.nan  
+        
+        while len(convexhull_lw) > 2 and cruz <= 0:
+            convexhull_lw = np.delete(convexhull_lw, -2, axis=0)
+                           
+    
+    return np.append(convexhull_up,convexhull_lw,axis=0), convexhull_up, convexhull_lw
+
+convexhull, convexhull_up, convexhull_lw = Incremental(cloud_1)
+
+print(convexhull_up[-1][0])
+ax = plt.gca()
+ax.scatter(cloud_1[:,0], cloud_1[:,1], c='black')
+ax.scatter(convexhull[:,0], convexhull[:,1], c='green')
+plt.show()
