a5d85fd0086d126cf7298eba0c80be2f1dd6863f
Juan Vivas <jdvivasf1@gmail.com>
2021-08-24 21:19:22 -0500

    Puntos de Prueba + orden

--- 2.trian_poli_mon_2d/TriangulateMonotonePolygon.py
+++ 2.trian_poli_mon_2d/TriangulateMonotonePolygon.py
@@ -5,3 +5,10 @@ array_1 = np.array([[1,1],[0,2],[-2,1]])
 array_2 = np.array([[0,0],[2,0],[2,2],[1,1],[-2,1]])
 
 points = np.array([array_1,array_2])
+test = points[1]
+
+def TriangulateMonotonePolygon(p):
+    p = p[np.lexsort((p[:, 0], -p[:, 1]), axis=0)]
+    return p
+
+p_ord = TriangulateMonotonePolygon(array_2)
